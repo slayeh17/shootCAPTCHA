7ff8d603e728bdcdc044607629088e8a2ec89d62
Debak Roy <debakroy7@gmail.com>
2024-08-16 11:50:08 +0530

    add endgame for finishing

--- script.js
+++ script.js
@@ -24,17 +24,22 @@ let render = Render.create({
   },
 });
 
-let collisionGroup = Body.nextGroup(true);  
+let collisionGroup = Body.nextGroup(true);
 
 let rockOptions = {
-  restitution: 0.3,
+  restitution: 0.9,
   collisionFilter: {
-    group: collisionGroup
+    group: collisionGroup,
   },
   render: {
     sprite: { texture: "./checkmark2.png", xScale: 0.05, yScale: 0.05 },
   },
 };
+
+let topBound = Bodies.rectangle(0, 0, 3000, 20, {
+  isStatic: true,
+  render: { fillStyle: "#f4f600" },
+});
 
 let rock = Bodies.polygon(170, 450, 8, 20, rockOptions);
 let anchor = { x: 170, y: 450 };
@@ -47,15 +52,14 @@ let elastic = Constraint.create({
   stiffness: 0.05,
 });
 
-let rimLeft = Bodies.rectangle(670, 200, 20, 10, {
+let rimLeft = Bodies.rectangle(680, 200, 20, 10, {
   isStatic: true,
   render: { fillStyle: "#FF0000" },
 });
-let rimRight = Bodies.rectangle(800, 200, 20, 10, {
+let rimRight = Bodies.rectangle(790, 200, 20, 10, {
   isStatic: true,
   render: { fillStyle: "#FF0000" },
 });
-
 
 let net = createCloth(670, 210, 8, 4, 10, 10, true, 4);
 
@@ -68,6 +72,18 @@ let obstacle = Bodies.rectangle(610, 250, 20, 200, {
   render: { fillStyle: "#f3f3f3" },
 });
 
+let reCaptchaBox = Bodies.rectangle(735, 320, 120, 30, {
+  isStatic: true,
+  isSensor: true,
+  render: {
+    sprite: {
+      texture: "./recaptcha.png",
+      xScale: 0.1,
+      yScale: 0.1,
+    },
+  },
+});
+
 Composite.add(engine.world, [
   obstacle,
   rock,
@@ -75,6 +91,8 @@ Composite.add(engine.world, [
   rimLeft,
   rimRight,
   net,
+  topBound,
+  reCaptchaBox,
 ]);
 
 Events.on(engine, "afterUpdate", function () {
@@ -153,4 +171,70 @@ function createCloth(
   Composites.mesh(cloth, columns, rows, crossBrace, constraintOptions);
   cloth.label = "Cloth Body";
   return cloth;
+}
+
+Events.on(engine, "collisionStart", function (event) {
+  var pairs = event.pairs;
+
+  console.log(pairs);
+  for (var i = 0, j = pairs.length; i != j; ++i) {
+    var pair = pairs[i];
+
+    if (pair.bodyA === reCaptchaBox) {
+      console.log("collided");
+      endGame();
+    } else if (pair.bodyB === reCaptchaBox) {
+      console.log("collided");
+      endGame();
+    }
+  }
+});
+
+function endGame() {
+  let reCaptchaBoxChecked = Bodies.rectangle(400, 320, 150, 50, {
+    isStatic: true,
+    render: {
+      sprite: {
+        texture: "./checked.png",
+        xScale: 0.3,
+        yScale: 0.3,
+      },
+    },
+  });
+
+  let ground = Bodies.rectangle(800, 600, 3000, 20, {
+    isStatic: true,
+    render: { fillStyle: "#FF0000" },
+  });
+  
+  const p = document.getElementById('message');
+  p.innerText = "Congralutaions, You are a human! Here are some balls to play with. 😀"
+  console.log(p);
+
+  let stack = Composites.stack(
+    100,
+    306 - 25.25 - 5 * 40,
+    15,
+    10,
+    0,
+    0,
+    function (x, y) {
+      return Bodies.circle(x, y, 20, {
+        restitution: 0.9
+      });
+    }
+  );
+
+  Composite.remove(engine.world, [
+    obstacle,
+    rock,
+    elastic,
+    rimLeft,
+    rimRight,
+    net,
+    topBound,
+    reCaptchaBox,
+  ]);
+
+  Composite.add(engine.world, [reCaptchaBoxChecked, ground, stack]);
 }
